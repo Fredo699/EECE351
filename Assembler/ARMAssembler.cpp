f7f5573d3b4e83f64e3feb8e860370ea10c88497
Caleb Wagoner <cwagone1@binghamton.edu>
2017-12-06 13:28:01 -0500

    updating

--- Assembler/ARMAssembler.cpp
+++ Assembler/ARMAssembler.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <iomanip>
 #include <bitset>
-
-std::ifstream infile("input.txt");
+#include <sstream>
 
 using namespace std;
 
-int main()
+ifstream infile;
+ifstream outfile;
+
+int main(void)
 {
-     const string CMD [27][2] = { {"AND", "EOR", "SUB", "RSB", "ADD", "ADC", "SBC", "RSC", "TST", "TEQ", "CMP", "CMN", "ORR", "BIC", "MVN", "MOV", "LSL", "LSR", "ASR", "RRX", "ROR", "MUL", "MLA", "UMULL", "UMLAL", "SMULL", "SMLAL"}, {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1110", "1111", "1101", "1101", "1101", "1101", "1101", "1101", "0 000", "0 001", "0 100", "0 101", "0 110", "0 111"} };
-     const string ST [4][2] = { {"LSL", "LSR", "ASR", "ROR"}, {"00", "01", "10", "11"} };
+     infile.open("input.txt");
+     
+     const string CMD [2][27] = { {"AND", "EOR", "SUB", "RSB", "ADD", "ADC", "SBC", "RSC", "TST", "TEQ", "CMP", "CMN", "ORR", "BIC", "MVN", "MOV", "LSL", "LSR", "ASR", "RRX", "ROR", "MUL", "MLA", "UMULL", "UMLAL", "SMULL", "SMLAL"}, {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1110", "1111", "1101", "1101", "1101", "1101", "1101", "1101", "0 000", "0 001", "0 100", "0 101", "0 110", "0 111"} };
+     const string ST [2][4] = { {"LSL", "LSR", "ASR", "ROR"}, {"00", "01", "10", "11"} };
      const string MEM[4] = {"LDR", "STR", "LDRB", "STRB"};
-     const string R1 [10][2] = { {"R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"}, {"0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010"} };
-     const string R2 [10][2] = { {"R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"}, {"0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010"} };
-     const string R3 [10][2] = { {"R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"}, {"0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010"} };
-     const string R4 [10][2] = { {"R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"}, {"0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010"} };
+     const string R1 [2][10] = { {"R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"}, {"0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010"} };
+     const string R2 [2][10] = { {"R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"}, {"0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010"} };
+     const string R3 [2][10] = { {"R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"}, {"0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010"} };
+     const string R4 [2][10] = { {"R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"}, {"0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010"} };
     
-     string cond = "0000";
-     string cmd = "0";
+    string a;
+    string b;
+    string c;
+    string d;
+    string e;
+    string f;
+    
+    string cond = "0000";
+    string instr = "00000000";
+    string cmd = "0";
 	string op = "0";
 	string s = "0";
 	string i = "0";
@@ -32,16 +45,18 @@ int main()
 	string rs = "0000";
 	string p = "0";
 	string u = "0";
-	string b = "0";
+	string bo = "0";
 	string w = "0";
 	string l = "0";
 	string imm12 = "000000000000";
 	string il = "00";
-	string imm24 "000000000000000000000000";
+	string imm24 = "000000000000000000000000";
 	string lb = "0";
-     string pc = "0000";
+    string pc = "0000";
+    
+    int toint = 0;
      
-     bool MEM = false;
+     bool MEM_set = false;
      bool i_set = false;
     
      cout << "Reading file." << endl << endl;
@@ -50,11 +65,11 @@ int main()
      {
 		
 		//determine cmd and a few other bits from read in file
-          for (z = 0; z <= 26; z++)
+          for (int z = 0; z <= 26; z++)
           {
-               if (a == CMD[z][1])
+               if (a == CMD[1][z])
                {
-                    cmd = CMD[z][2];
+                    cmd = CMD[2][z];
 				
 				op = "00";
 				
@@ -79,7 +94,7 @@ int main()
 				else if (z == 18)
 				{
 					i = "0";
-					sh "10";
+					sh = "10";
 				}
 				else if (z == 19)
 				{
@@ -101,7 +116,7 @@ int main()
           if (a == MEM[0] || a == MEM[2])
           {
                op = "01";
-               MEM = true;
+               MEM_set = true;
                
                if (f.at(0) == "#")
                {
@@ -119,23 +134,23 @@ int main()
                     }
                     
                     p = "0";
-                    b = "0";
+                    bo = "0";
                     w = "0";
                     l = "0";
                }
-               else if
+               else
                {
-                    for (z = 0; z <= 3; z++)
+                    for (int z = 0; z <= 3; z++)
                     {
-                         if (d == R3[z][1])
+                         if (d == R3[1][z])
                          {
                               i = "1";
-                              rm = R3[z][2];
+                              rm = R3[2][z];
                               shamt5 = "0000";
                               sh = "0";
                               p = "1";
                               u = "1";
-                              b = "0";
+                              bo = "0";
                               w = "0";
                               l = "0";
                               z = 3;
@@ -146,13 +161,16 @@ int main()
                if (i=="0")
                {
                     imm12 = f;
-                    imm12 = std::string binary = std::bitset<12>(imm12).to_string();
+                    
+                    stringstream geek(imm12);
+                    geek >> toint;
+                    imm12 = bitset<12>(toint).to_string();
                }
           }
           else if (a == MEM[1] || a == MEM[3])
           {
                op = "01";
-               MEM = true;
+               MEM_set = true;
                
                if (f.at(0) == "#")
                {
@@ -170,23 +188,23 @@ int main()
                     }
                     
                     p = "0";
-                    b = "0";
+                    bo = "0";
                     w = "0";
                     l = "1";
                }
-               else if
+               else
                {
-                    for (z = 0; z <= 3; z++)
+                    for (int z = 0; z <= 3; z++)
                     {
-                         if (d == R3[z][1])
+                         if (d == R3[1][z])
                          {
                               i = "1";
-                              rm = R3[z][2];
+                              rm = R3[2][z];
                               shamt5 = "0000";
                               sh = "0";
                               p = "1";
                               u = "1";
-                              b = "0";
+                              bo = "0";
                               w = "0";
                               l = "1";
                               z = 3;
@@ -197,7 +215,10 @@ int main()
                if (i=="0")
                {
                     imm12 = f;
-                    imm12 = std::string binary = std::bitset<12>(imm12).to_string();
+                    
+                    stringstream geek(imm12);
+                    geek >> toint;
+                    imm12 = bitset<12>(toint).to_string();
                }
           }
           
@@ -209,41 +230,45 @@ int main()
                il = "0";
                pc = "1100";
                
-               imm24 = std::string binary = std::bitset<24>(pc).to_string();
+               stringstream geek(pc);
+               geek >> toint;
+               imm24 = bitset<24>(toint).to_string();
           }
           else if (a == "BL")
           {
                op = "10";
-               il = "1":
+               il = "1";
                pc = "0011";
                
-               imm24 = std::string binary = std::bitset<24>(pc).to_string();
+               stringstream geek(pc);
+               geek >> toint;
+               imm24 = bitset<24>(toint).to_string();
           }
 		
 		//determine and set rn and rd from read in values
-          if(MEM == false)
+          if(MEM_set == false)
           {
-               for (z = 0; z <= 9: z++)
+               for (int z = 0; z <= 9; z++)
                {
-                    if (b == R1[z][1])
+                    if (b == R1[1][z])
                     {
-                         rd = R1[z][2];
+                         rd = R1[2][z];
                     }
-                    if (c == R2[z][1])
+                    if (c == R2[1][z])
                     {
-                         rn = R2[z][2];
+                         rn = R2[2][z];
                     }
-                    if (d == R3[z][1])
+                    if (d == R3[1][z])
                     {
-                         rm = R3[z][2];
+                         rm = R3[2][z];
                          i = "0";
                          i_set = true;
                          shamt5 = "0";
                          sh = "0";
                     }
-                    if (f == R4[z][1])
+                    if (f == R4[1][z])
                     {
-                         rs = R4[z][2];
+                         rs = R4[2][z];
                     }
                }
           }
@@ -259,15 +284,17 @@ int main()
                     d.erase(0);
                     rot = "0000";
 			
-                    imm8 = std::string binary = std::bitset<8>(lb).to_string();
-               } 
+			        stringstream geek(lb);
+                    geek >> toint;
+                    imm8 = bitset<8>(toint).to_string();
+		        } 
           }
           
-          for (z = 0; z <= 3; z++)
+          for (int z = 0; z <= 3; z++)
           {
-               if (e == ST[z][1])
+               if (e == ST[1][z])
                {
-                    sh = ST[z][2];
+                    sh = ST[2][z];
                     z = 3;
                }
           }          
@@ -297,11 +324,11 @@ int main()
           {
                if (i == "1")
                {
-                    cout << cond << op << i << p << u << b << w << l << rn << rd << shamt << sh << "0" << rm << endl;
+                    cout << cond << op << i << p << u << bo << w << l << rn << rd << shamt5 << sh << "0" << rm << endl;
                }
                else if (i == "0")
                {
-                    cout << cond << op << i << p << u << b << w << l << rn << rd << imm12 << endl;
+                    cout << cond << op << i << p << u << bo << w << l << rn << rd << imm12 << endl;
                } 
           }
           else if (op == "10")
@@ -309,6 +336,8 @@ int main()
                cout << cond << op << "1" << il << imm24 << endl;
           }
 	}
+     
+     infile.close();
      
     return 0;
 }
